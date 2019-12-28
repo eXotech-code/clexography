d135bb9bf1f190ca8f8b5a8077838114b4bf863d
Jacob <remotekonk@gmail.com>
2019-12-28 23:21:57 +0100

    Streamlined quoting for strings. Still don't know how to change user prompts.

--- clexography.py
+++ clexography.py
@@ -9,7 +9,7 @@ if selectAction == 'encode':
     filePath = input('Input path of the image file: ')
 
     # converter
-    with open(filePath, "rb") as imageFile:
+    with open(filePath, 'rb') as imageFile:
         imagestr = base64.b64encode(imageFile.read())   # decodes image raw data into string
         print(imagestr)                                 # prints the text from image data
 
@@ -17,7 +17,7 @@ if selectAction == 'encode':
     confirm = input('Do you want to save this text as .txt file? (Y/n): ')
     if confirm == 'Y' or 'y' or 'yes' or 'yep' or 'Yes' or "yeah mate, brin' it on":
         savePath = input('Input path of the new text file (recomended .txt): ')
-        text_file = open(savePath, "wb")                            # opens the specified file path for editing
+        text_file = open(savePath, 'wb')                            # opens the specified file path for editing
         n = text_file.write(imagestr)                               # inserts image string to the text file
         text_file.close()
         print('Text file has been saved in: ' + (savePath))          # prints saved file's path
@@ -30,7 +30,7 @@ elif selectAction == 'decode':
     filePath = input('Input path of the text file: ')
 
     # program reads the text file
-    with open(filePath, "rb") as textFile:
+    with open(filePath, 'rb') as textFile:
         imagestrRaw = textFile.read()
 
     # converting bytes to utf-8
@@ -40,11 +40,11 @@ elif selectAction == 'decode':
     folderPath = input('Please input directory to save decoded-image.png in (enter "." to save to current working directory): ')
 
     # writing converted string to image file
-    with open(folderPath + "/decoded-image.png", "wb") as imageFile:        # path to the file is predetermined (will change in the future)
+    with open(folderPath + '/decoded-image.png', 'wb') as imageFile:        # path to the file is predetermined (will change in the future)
         imageFile.write(imagestr)
         imageFile.close()
 
     print('Operation completed successfully!')
-    print('File saved in ' + folderPath + "/decoded-image.png")
+    print('File saved in ' + folderPath + '/decoded-image.png')
 else:
     print('ERROR: function not determined correctly. Please run and try again!')
