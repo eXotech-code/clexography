f8f0cedec247683002eb63dcbd417c2b9aecdf0b
juliusz <juliusz@muskala.eu>
2019-12-30 00:12:44 +0100

    Cleanup clexography.py

--- clexography.py
+++ clexography.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # imports
 import imgconv
 import txtconv
@@ -5,18 +7,20 @@ import txtconv
 def main():
     # select img >> txt or txt >> img
     print('Welcome to Clexography!')
-    selectAction = input('Do you want to encode (image >> text) or decode (text >> image)? (e/d): ')
-    if (selectAction == 'e') or (selectAction == 'E') or (selectAction == 'encode') or (selectAction == 'Encode'):
+    selectAction = input('Do you want to encode (image >> text) or decode (text >> image)? (e/d): ').lower()
+
+    if selectAction in ['e', 'encode']:
         imgconv.imageText()
-    elif (selectAction == 'd') or (selectAction == 'D') or (selectAction == 'decode') or (selectAction == 'Decode'):
+    elif selectAction in ['d', 'decode']:
         txtconv.textImage()
+    elif selectAction == 'exit':
+        print('Exiting...')
     else:
         print('ERROR: function "' + (selectAction) + '" is not a valid function. Please run and try again!')
-        rerun = input('Do you want to rerun the program? (Y/n): ')
-        if (rerun == 'y') or (rerun == 'Y') or (rerun == 'yes') or (rerun =='Yes'):
+        rerun = input('Do you want to rerun the program? (Y/n): ').lower()
+        if rerun in ['y', 'yes']:
             main()
         else:
             print('Exiting...')
-
 
 main()
--- txtconv.py
+++ txtconv.py
@@ -1,24 +1,23 @@
 # imports
 import base64
 
-def textImage():
-    # user inputs text file path
-    filePath = input('Input path of the text file: ')
+def textImage():    
     
+    fileName = input('Input path of the text file: ')   # user inputs text file path
     # program reads the text file
-    with open(filePath, 'rb') as textFile:
-        imagestrRaw = textFile.read()
-    
+    with open(fileName, 'rb') as textFile:
+        imageStr = textFile.read()
+             
     # converting bytes to utf-8
-    imagestr = base64.b64decode(imagestrRaw)
-    
-    # user inputs folder to which the converted image file will be saved
-    folderPath = input('Please input directory to save decoded-image.png in (enter "." to save to current working directory): ')
-    
+    imageStr = base64.b64decode(imageStr)
+            
+    # user inputs directory to which the converted image file will be saved
+    dirPath = input('Please input directory to save decoded-image.png in (enter "." to save to current working directory): ')
+            
     # writing converted string to image file
-    with open(folderPath + '/decoded-image.png', 'wb') as imageFile:        # path to the file is predetermined (will change in the future)
-        imageFile.write(imagestr)
+    with open(dirPath + '/decoded-image.png', 'wb') as imageFile:        # path to the file is predetermined (will change in the future)
+        imageFile.write(imageStr)
         imageFile.close()
-        
+            
     print('Operation completed successfully!')
-    print('File saved in ' + folderPath + '/decoded-image.png')
+    print('File saved in ' + dirPath + '/decoded-image.png')
