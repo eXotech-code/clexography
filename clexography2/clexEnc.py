1ddb3f0f2ba3f8fa238df414d178c66be0a1010f
breakingbar <30392252+breakingbar@users.noreply.github.com>
2019-12-30 20:31:15 +0100

    Add files via upload
    Added clexography 2

---
+++ clexography2/clexEnc.py
@@ -0,0 +1,10 @@
+import base64
+import clexography
+
+readFile = clexography.readFile
+writeFile = clexography.writeFile
+
+def encode():
+    print('encoding')
+    print(readFile)
+    print(writeFile)
