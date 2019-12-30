7163ffef9604251b7b7e8c0a7875a437735397cd
Jacob <remotekonk@gmail.com>
2019-12-30 17:34:16 +0100

    Optimized extension dedection.

--- txtconv.py
+++ txtconv.py
@@ -5,33 +5,28 @@ import base64
 import sys
 
 def textImage():    
-    fileName = input('Input path of the text file: ')   # user inputs text file path
+    fileName = input('Input path of the text file: ')       # user inputs text file path
     # program reads the text file
     with open(fileName, 'rb') as textFile:
         imageStr = textFile.read()
-    # converting bytes to utf-8
+    # decoding text
     imageStr = base64.b64decode(imageStr)
             
     # user inputs path to the new image file
     dirPath = input('Input the name of the new image file: ')
-    if '.' in dirPath:                                  # program checks for file extension
-        # writing converted string to image file
-        with open(dirPath, 'wb') as imageFile:
-            imageFile.write(imageStr)
-            imageFile.close()
-
-        print('Operation completed successfully!')
-        print('File saved in ' + dirPath)
-    else:
+    if not '.' in dirPath:                                  # program checks for file extension
         ext = input('Please input file extension: ')
-        if '.' in ext:
-            # writing converted string to image file with file extension
-            with open(dirPath + ext, 'wb') as imageFile:
-                imageFile.write(imageStr)
-                imageFile.close()
-
-            print('Operation completed successfully!')
-            print('File saved in ' + dirPath + ext)
-        else:
-            print('ERROR: This is not a corret file extension. Exiting...')
+        if not '.' in ext:
+            print('ERROR: This is not a correct file extension. Exiting...')
             sys.exit()
+
+        # adding file extension to dirPath
+        dirPath = dirPath + ext
+        
+    # writing converted string to image file
+    with open(dirPath, 'wb') as imageFile:
+        imageFile.write(imageStr)
+        imageFile.close()
+
+    print('Operation completed successfully!')
+    print('File saved in ' + dirPath)
