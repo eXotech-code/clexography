25b7b26cea81a132d44ab83b8ec0441bd6a28ab9
Jacob <remotekonk@gmail.com>
2019-12-30 16:25:16 +0100

    Added file extension detection

--- txtconv.py
+++ txtconv.py
@@ -12,13 +12,26 @@ def textImage():
     # converting bytes to utf-8
     imageStr = base64.b64decode(imageStr)
             
-    # user inputs directory to which the converted image file will be saved
-    dirPath = input('Input the name of the new image file (same extension as original): ')
-            
-    # writing converted string to image file
-    with open(dirPath, 'wb') as imageFile:        # path to the file is predetermined (will change in the future)
-        imageFile.write(imageStr)
-        imageFile.close()
-            
-    print('Operation completed successfully!')
-    print('File saved in ' + dirPath)
+    # user inputs path to the new image file
+    dirPath = input('Input the name of the new image file: ')
+    if '.' in dirPath:                                  # program checks for file extension
+        # writing converted string to image file
+        with open(dirPath, 'wb') as imageFile:
+            imageFile.write(imageStr)
+            imageFile.close()
+
+        print('Operation completed successfully!')
+        print('File saved in ' + dirPath)
+    else:
+        ext = input('Please input file extension: ')
+        if '.' in ext:
+            # writing converted string to image file with file extension
+            with open(dirPath + ext, 'wb') as imageFile:
+                imageFile.write(imageStr)
+                imageFile.close()
+
+            print('Operation completed successfully!')
+            print('File saved in ' + dirPath + ext)
+        else:
+            print('ERROR: This is not a corret file extension. Exiting...')
+            sys.exit()
