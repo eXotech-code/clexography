19720203ffad50199f8da318fd298850f4660dfa
Jacob <remotekonk@gmail.com>
2019-12-30 18:01:40 +0100

    Applied extension detection to imgconv.py

--- imgconv.py
+++ imgconv.py
@@ -2,10 +2,18 @@
 
 # imports
 import base64
+import sys
 
 def imageText():
     # user specifies the file path
     filePath = input('Input path of the image file: ')
+    # extension checker
+    if not '.' in filePath:
+        imgExt = input('Please input file extension: ')
+        if not '.' in imgExt:
+            print('ERROR: This is not a correct file extension. Exiting...')
+            sys.exit()
+        filePath = filePath + imgExt
     
     # converter
     with open(filePath, 'rb') as imageFile:
@@ -14,7 +22,16 @@ def imageText():
     # saving as text file
     confirm = input('Do you want to save this as a text file? (Y/n): ').lower()
     if confirm in ['y', 'yes']:
-        savePath = input('Input the name of the new text file (recomended .txt): ')
+        savePath = input('Input the name of the new text file: ')
+        # extension checker
+        if not '.' in savePath:
+            extChoice = input('Do you want to save this file with no extension? (Y/n): ').lower()
+            if extChoice in ['n', 'no']:
+                ext = input('Please input file extension: ')
+                if not '.' in ext:
+                    print('ERROR: This is not a correct file extension. Exiting...')
+                    sys.exit()
+                savePath = savePath + ext
         text_file = open(savePath, 'wb')                            # opens the specified file name for editing
         text_file.write(imagestr)                                   # inserts image string to the text file
         text_file.close()
