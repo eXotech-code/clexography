9f8a91ceaad40f3dd44236829586ee9c181048eb
juliusz <juliusz@muskala.eu>
2020-01-01 19:19:14 +0100

    Little cleanup

--- clexography.py
+++ clexography.py
@@ -5,15 +5,14 @@ import clexEnc
 import clexDec
 
 version = '2.0'
-name = 'Clexography'
+name = 'clexography'
 usage = name+' '+' '+version
 
 #command line arguments
-
 parser = argparse.ArgumentParser(prog=name, usage=usage)
 parser.add_argument("-v", "--version", help="show %(prog)s version", action="store_true")
 actionGroup = parser.add_mutually_exclusive_group()
-actionGroup.add_argument("-e", "--encode", help="encode image", nargs=2)    #encode argument takes 2 actions (read write)s
+actionGroup.add_argument("-e", "--encode", help="encode image", nargs=2)    #encode argument takes 2 actions (read write)
 actionGroup.add_argument("-d", "--decode", help="decode image", nargs=2)    #decode argument takes 2 actions (read write)
 
 args = parser.parse_args()
