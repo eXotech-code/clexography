089680275da78b785f8aeef9f281cafa4532f27b
juliusz <juliusz@muskala.eu>
2020-01-01 17:12:06 +0100

    Removed functions from the main file

--- clexography.py
+++ clexography.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
-import base64
+import clexEnc
+import clexDec
 
 version = '2.0'
 name = 'Clexography'
@@ -12,7 +13,7 @@ usage = name+' '+' '+version
 parser = argparse.ArgumentParser(prog=name, usage=usage)
 parser.add_argument("-v", "--version", help="show %(prog)s version", action="store_true")
 actionGroup = parser.add_mutually_exclusive_group()
-actionGroup.add_argument("-e", "--encode", help="encode image", nargs=2)    #encode argument takes 2 actions (read write)
+actionGroup.add_argument("-e", "--encode", help="encode image", nargs=2)    #encode argument takes 2 actions (read write)s
 actionGroup.add_argument("-d", "--decode", help="decode image", nargs=2)    #decode argument takes 2 actions (read write)
 
 args = parser.parse_args()
@@ -22,16 +23,8 @@ if args.version:
 elif args.encode:
     readFile = args.encode[0]
     writeFile = args.encode[1]
-    with open(readFile, 'rb') as inFile:
-        editStr = base64.b64encode(inFile.read())
-    with open(writeFile, 'wb') as outFile:
-        outFile.write(editStr)
-    print('Text file saved in ' + writeFile)
+    clexEnc.clexEnc(readFile, writeFile)
 elif args.decode:
     readFile = args.decode[0]
     writeFile = args.decode[1]
-    with open(readFile, 'rb') as inFile:
-        editStr = base64.b64decode(inFile.read())
-    with open(writeFile, 'wb') as outFile:
-        outFile.write(editStr)
-    print('Image saved in ' + writeFile)
+    clexDec.clexDec(readFile, writeFile)
