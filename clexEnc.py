cea53ad7060e16fcd5681de529a6aaf6d005aece
Jacob <exotech.code@gmail.com>
2020-01-04 15:26:46 +0100

    Changed names of convertion scripts.

--- clexEnc.py
+++
@@ -1,8 +0,0 @@
-import base64
-
-def clexEnc(readFile, writeFile):
-    with open(readFile, 'rb') as inFile:
-        editStr = base64.b64encode(inFile.read())
-    with open(writeFile, 'wb') as outFile:
-        outFile.write(editStr)
-    print('Text file saved in ' + writeFile)
