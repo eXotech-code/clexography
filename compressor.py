40ebc1794b0ba930b4083914e851e116835078cb
Jacob <exotech.code@gmail.com>
2020-01-05 21:04:39 +0100

    Compressor now groups repeated characters.
    Now it just needs to put a prefix before each character that gets repeated. For example:
    If the string is: "uuuuuAAAAGGGhJJJJKKK"
    Compressor should output:
    "5xu4xA3xGh4xJ3xK"

--- compressor.py
+++ compressor.py
@@ -1,12 +1,31 @@
-import collections
+import re
+
+# transforming list into string
+def listToString(repeatsCut):
+    finalStr = ''
+    for ele in repeatsCut:
+        finalStr += ele
+    return finalStr
+
+    
 
 # compression function
 def compress(startFile, finalFile):
     with open(startFile, 'r') as inFile:                # program opens startFile as a string
         startStr = inFile.read()
 
-    finalStr = collections.Counter(startStr)
-    print(finalStr)
+    repeats = re.findall(r'((.)\2*)', startStr)
+    # it this stage i have [('111', '1'), ('2', '2'), ('3', '3'), ('4', '4')] in l
+
+    # now I am keeping only the first value from the tuple of each list
+    repeatsCut = [x[0] for x in repeats]
+
+    print(repeatsCut)
+
+    finalStr = listToString(repeatsCut)
+
+    with open(finalFile, 'w') as outFile:
+        outFile.write(finalStr)
 
 startFile = 'test.txt'
 finalFile = 'compressed.txt'
