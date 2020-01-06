fb107a36fc4ef811f42653d906f192523bed5220
Jacob <exotech.code@gmail.com>
2020-01-06 19:03:29 +0100

    Some work on the compressor.
    Compressor now deletes duplicates, but still cannot write number of duplicated characters.

--- compressor.py
+++ compressor.py
@@ -5,10 +5,17 @@ def compress(startFile):
     with open(startFile, 'r') as textFile:                # program opens startFile as a string
         startStr = textFile.read()
 
-    repeats = re.findall(r'((.)\2*)', startStr)
-    repeatsCut = [x[0] for x in repeats]
+    print('startStr:\n' + startStr[0:100])
 
-    print(repeatsCut[0:100])
+    repeats = re.sub(r'[^\w\s]|(.)(?=\1)', '', startStr)
 
+    print('repeats:\n' + repeats[0:100])
+
+    startFile = 'compressed-' + startFile
+
+    with open(startFile, 'w') as outFile:
+        outFile.write(repeats)
+    print('Text file saved as ' + startFile)
+    
 startFile = input('[DEV] Input startFile: ')
 compress(startFile)
