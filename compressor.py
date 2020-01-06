b21e6b62956100fc42f8e1beb3d3c47e3c0e2199
Jakub <remotekonk@gmail.com>
2020-01-06 23:19:02 +0100

    Compressor now works as expected.

--- compressor.py
+++ compressor.py
@@ -1,5 +1,3 @@
-import re
-
 # compression function
 def compress(startFile):
     with open(startFile, 'r') as textFile:                # program opens startFile as a string
@@ -7,7 +5,21 @@ def compress(startFile):
 
     print('startStr:\n' + startStr[0:100])
 
-    repeats = re.sub(r'[^\w\s]|(.)(?=\1)', '', startStr)
+    repeats = ''
+    count = 1
+
+    repeats += startStr[0]
+
+    for i in range(len(startStr)-1):
+        if(startStr[i] == startStr[i+1]):
+            count += 1
+        else:
+            if(count > 1):
+                repeats += str(count)
+            repeats += startStr[i+1]
+            count = 1
+    if(count > 1):
+        repeats += str(count)
 
     print('repeats:\n' + repeats[0:100])
 
