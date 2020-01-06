171dc5ea745e9e0006f4245c5fc86ae43a9568eb
Jacob <exotech.code@gmail.com>
2020-01-06 14:05:27 +0100

    Extension checker now works also when user wants to save to current working directory

--- functions.py
+++ functions.py
@@ -1,14 +1,22 @@
 # extension checker
+
+def addExt(sys, writeFile):
+    extChoice = input('Do you want to save this file with no extension? (Y/n): ').lower()
+    if extChoice in ['n', 'no']:
+        ext = input('Please input file extension: ')
+        if not '.' in ext:
+            print('ERROR: This is not a correct file extension. Exiting...')
+            sys.exit()
+        writeFile = writeFile + ext
+        return writeFile                # writeFile with extension
+    else:
+        return writeFile
+
 def extCheck(sys, writeFile):
     if not '.' in writeFile:
-        extChoice = input('Do you want to save this file with no extension? (Y/n): ').lower()
-        if extChoice in ['n', 'no']:
-            ext = input('Please input file extension: ')
-            if not '.' in ext:
-                print('ERROR: This is not a correct file extension. Exiting...')
-                sys.exit()
-            writeFile = writeFile + ext
-            return writeFile                                                        # writeFile with extension
+        return addExt(sys, writeFile)
+    elif writeFile[0] == '.':           # for when user wants to write file to current directory
+        return addExt(sys, writeFile)
     # if extension checker finds extension, it returns unchanged writeFile string
     else:
         return writeFile
