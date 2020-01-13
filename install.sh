6c8b87afd952a6fe8cb66c0bd0db8eb8519ce33d
Jacob <exotech.code@gmail.com>
2020-01-13 11:27:13 +0100

    install.sh fix

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ command -v pip3 >/dev/null 2>&1 || continue
 command -v pip >/dev/null 2>&1 || { echo >&2 "Pip not found on this system. Please install pip and try again."; exit 1; }
 echo Pip has been found. continuing...
 echo -e "Installing setup tools...\n"
-sudo python -m pip install --upgrade pip setuptools wheel
+sudo python -m pip install --upgrade pip setuptools
+sudo python -m install --upgrade wheel
 echo -e "Creating a python package...\n"
 python3 setup.py sdist bdist_wheel
 echo Package has been created.
