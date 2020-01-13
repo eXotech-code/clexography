0ebdfe822e1f5976968954d62fdff754e1e91fe5
Jacob <exotech.code@gmail.com>
2020-01-13 10:17:29 +0100

    Reverted changes for setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = 'clexography',
-    version = 'v0.2.5',
+    version = 'v0.2',
     scripts = ['clexography'] ,
     author = "clexography",
     author_email = "exotech.code@gmail.com",
